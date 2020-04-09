bb0b7745f7062952b97c3b7d82824f6cd035a1e0
Anabel Carnerero Blas <anabel.carnerero@juanxxiii.net>
2020-04-09 18:19:33 +0200

    Inicio de la app creada, LOGGING

--- TFC_2/TFC_2/MainWindow.xaml.cs
+++ TFC_2/TFC_2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,55 @@ namespace TFC_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            Boolean encontrado = false;
+            string cadenaConexion = "server=localhost;database=lynse;uid=root;pwd=\"\";";
+            MySqlConnection conexionBBDD = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conexionBBDD.Open();
+
+                MySqlDataReader reader = null;
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM usuario", conexionBBDD);
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    if (reader.GetString(2).Equals(txtUser.Text) && reader.GetString(3).Equals(txtPassword.Password))
+                    {
+                        encontrado = true;
+                    }
+                }
+                
+                conexionBBDD.Close();
+                if (encontrado)
+                {
+                    Ventana_Home ventana_Home = new Ventana_Home();
+                    ventana_Home.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("El nombre de usuario y/o contraseña que has introducido no pertenece a ninguna cuenta. \nComprueba tu nombre y contraseña de usuario y vuelve a intentarlo.");
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
