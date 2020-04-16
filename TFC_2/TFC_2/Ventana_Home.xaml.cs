e833c8bfc2e2d3b4c7831d544759ef05c367cd82
AnabelCarnerero <50024085+AnabelCarnerero@users.noreply.github.com>
2020-04-16 19:19:20 +0200

    Interfaz ventana home acabada

--- TFC_2/TFC_2/Ventana_Home.xaml.cs
+++ TFC_2/TFC_2/Ventana_Home.xaml.cs
@@ -12,6 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Syncfusion.UI.Xaml.ProgressBar;
+
+using LiveCharts;
+using LiveCharts.Wpf;
+using MySql.Data.MySqlClient;
+using System.Data;
+
 namespace TFC_2
 {
     /// <summary>
@@ -22,11 +28,90 @@ namespace TFC_2
         public Ventana_Home()
         {
             InitializeComponent();
-        }
 
-        private void Image_MouseMove(object sender, MouseEventArgs e)
-        {
             
+            string cadenaConexion = "server=localhost;database=lynse;uid=root;pwd=\"\";";
+            MySqlConnection conexionBBDD = new MySqlConnection(cadenaConexion);
+            try
+            {
+                
+                conexionBBDD.Open();
+
+                //MySqlDataReader reader = null;
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM empleados", conexionBBDD);
+                //reader = cmd.ExecuteReader();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+
+                da.Fill(ds);
+
+                grid_AlbaranesVenta_Copy1.ItemsSource = ds.Tables[0].DefaultView;
+
+                conexionBBDD.Close();
+                
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            // -------------------------------------------------------------------------------------------------------------------- GRAFICA DE BARRAS
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2015",
+                    Values = new ChartValues<double> { 10, 50, 39, 50 }
+                }
+            };
+
+            //adding series will update and animate the chart automatically
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "2016",
+                Values = new ChartValues<double> { 11, 56, 42 }
+            });
+
+            //also adding values updates and animates the chart automatically
+            SeriesCollection[1].Values.Add(48d);
+
+            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
+            Formatter = value => value.ToString("N");
+
+            DataContext = this;
+
         }
+        // -------------------------------------------------------------------------------------------------------------------- GRAFICA DE BARRAS
+
+            /*
+        public void gird_Prueba()
+        {
+            string cadenaConexion = "server=localhost;database=lynse;uid=root;pwd=\"\";";
+            MySqlConnection conexionBBDD = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conexionBBDD.Open();
+
+                MySqlDataReader reader = null;
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM usuario", conexionBBDD);
+                reader = cmd.ExecuteReader();
+
+
+                grid_AlbaranesVenta_Copy1.ItemsSource = reader.ToString();
+
+                conexionBBDD.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        */
+
+        public CartesianChart grafica1 { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
     }
 }
